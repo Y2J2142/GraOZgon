438218b1fc39efe99f96899685ab5c6e28b44655
Y2j2142 <Y2J2142@hotmail.com>
2019-04-14 13:00:41 +0200

    Drinking is coming

--- GraOZgon/GraOZgon/GraOZgon.fs
+++ GraOZgon/GraOZgon/GraOZgon.fs
@@ -8,7 +8,9 @@ let main argv =
     let mutable players = argv |> Array.map (fun n -> {Name = n; Hand = [||]})
     while players.Length > 1 do
         players <- players |> Array.map RollAndSort
-        players |> Array.map (fun p -> (printfn "%s %s"(Print p)  (PrintScore p))) |> ignore
+        players |> Array.sortBy (fun p ->  Score p,  (Array.map (~-) p.Hand)) 
+                |> Array.map (fun p -> (printfn "%s %A"(Print p)  (PrintScore p))) 
+                |> ignore
         let playerToRemove = Console.ReadLine ()
         players <- players |> Array.filter (fun p -> p.Name <> playerToRemove) 
         Console.Clear()
